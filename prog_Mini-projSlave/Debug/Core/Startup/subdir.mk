714d93f51062a0e753a54f346cbb61d3e37bf9a3
No-O-oT <nicolas.trosino@ens-paris-saclay.fr>
2021-04-20 14:34:21 +0200

    Départ aléatoire

--- prog_Mini-projSlave/Debug/Core/Startup/subdir.mk
+++ prog_Mini-projSlave/Debug/Core/Startup/subdir.mk
@@ -1,6 +1,5 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -15,6 +14,6 @@ S_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/Startup/startup_stm32f746nghx.o: ../Core/Startup/startup_stm32f746nghx.s Core/Startup/subdir.mk
+Core/Startup/startup_stm32f746nghx.o: ../Core/Startup/startup_stm32f746nghx.s
 	arm-none-eabi-gcc -mcpu=cortex-m7 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Core/Startup/startup_stm32f746nghx.d" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
